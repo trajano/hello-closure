2808ccaf4c6c2ba5b8d105765e52a3d2f4dc6904
Archimedes Trajano <arch@trajano.net>
2013-05-29 03:20:08 -0400

    Changes to make things work with ClosureBuilder
    https://developers.google.com/closure/library/docs/closurebuilder
    
    Some differences from the example provided by Google are:
    * kept the original file names hello.html and hello.js
    * Did not use the embedded script tag

--- hello.js
+++ hello.js
@@ -1,8 +1,12 @@
+goog.provide('myproject.start');
+
 goog.require('goog.dom');
 
-function sayHi() {
+myproject.start = function () {
     "use strict";
     var newHeader = goog.dom.createDom('h1', {'style': 'background-color:#EEE'},
         'Hello world!');
     goog.dom.appendChild(document.body, newHeader);
 }
+
+goog.exportSymbol('myproject.start', myproject.start);
